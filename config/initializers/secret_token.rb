86a9a13a12385a58b9b4de1dd7ec5a37deabd5a2
Corey Woodcox <corey.woodcox@gmail.com>
2013-02-09 11:36:38 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetMood::Application.config.secret_token = 'c9650575769ccb5b66c718bc8384580031baecf87d97ee5a2c0852136f390ffd1cf1d0ebf6b58b5b848e6d2d5158f11b739587589806b862241d8943aae0e60d'
