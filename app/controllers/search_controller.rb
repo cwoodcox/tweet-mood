8cbc428e1fc9f5475bbc42abd4778299e126e571
Corey Woodcox <corey.woodcox@gmail.com>
2013-02-09 14:09:40 -0700

    there, we get tweets from twitter based on a city

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,9 +5,25 @@ class SearchController < ApplicationController
 
   def search
     city_1_geo = Twitter.geo_search(query: params[:search][:city_1], granularity: 'city')
-    city_1_geo = city_1_geo.first.bounding_box.coordinates.first.collect(&:reverse) # Why do these come in such tiny boxes and BACKWARDS?
-    city_1_gem = city_1_geo.reduce
-    city_1_tweets = Twitter.search(params[:search][:query], geocode: city_1_geo).collect(&:statuses).collect(&:text)
-    city_2_tweets = Twitter.search(params[:search][:query], geocode: city_2_geo).collect(&:statuses).collect(&:text)
+    city_1_geo = average_square get_coords(city_1_geo)
+
+    city_2_geo = Twitter.geo_search(query: params[:search][:city_2], granularity: 'city')
+    city_2_geo = average_square get_coords(city_2_geo)
+
+    city_1_tweets = Twitter.search(params[:search][:query], geocode: city_1_geo.join(',') + ",25mi").statuses.collect(&:text)
+    city_2_tweets = Twitter.search(params[:search][:query], geocode: city_2_geo.join(',') + ",25mi").statuses.collect(&:text)
+  end
+
+  def average_square array
+    array.reduce([]) do |ret, coords|
+      ret = [coords[0] + ret[0].to_f, coords[1] + ret[1].to_f]
+    end.collect do |coords|
+      coords / array.length
+    end
+  end
+
+  def get_coords array
+    # Why do these come in such tiny boxes and BACKWARDS?
+    array.first.bounding_box.coordinates.first.collect(&:reverse)
   end
 end
