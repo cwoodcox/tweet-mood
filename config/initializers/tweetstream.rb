decc2bdd0417c5a6b729f5cd74dd3131b614ab00
Corey Woodcox <corey.woodcox@gmail.com>
2013-02-09 12:58:29 -0700

    twitter app auth

---
+++ config/initializers/tweetstream.rb
@@ -0,0 +1,5 @@
+TweetStream.configure do |config|
+  config.consumer_key       = 'xRWvopX5jfUZMeuYkkJg'
+  config.consumer_secret    = 'RbdYBsK2PRpPkPkVwOLDQ4ZNYxEt1sM9qjWJCKb7iM'
+  config.auth_method        = :oauth
+end
