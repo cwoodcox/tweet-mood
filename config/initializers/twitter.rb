4e073c0ecb4bfe91378258cb1323f72b91d65871
Corey Woodcox <corey.woodcox@gmail.com>
2013-02-09 13:08:28 -0700

    move config and fix it iup

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-TweetStream.configure do |config|
+Twitter.configure do |config|
   config.consumer_key       = 'xRWvopX5jfUZMeuYkkJg'
   config.consumer_secret    = 'RbdYBsK2PRpPkPkVwOLDQ4ZNYxEt1sM9qjWJCKb7iM'
   config.auth_method        = :oauth
