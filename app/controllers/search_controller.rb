c4d4cba566b9f3e6ce7eb4f89018a2ad6677ff6c
Corey Woodcox <corey.woodcox@gmail.com>
2013-02-09 14:45:14 -0700

    yay it does something!

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,22 +4,27 @@ class SearchController < ApplicationController
   end
 
   def search
-    city_1 = Twitter.geo_search(query: params[:search][:city_1], granularity: 'city')
+    city_1 = Twitter.geo_search(query: params[:search][:city_1], granularity: 'city').first
     city_1_geo = average_square get_coords(city_1)
 
-    city_2 = Twitter.geo_search(query: params[:search][:city_2], granularity: 'city')
+    city_2 = Twitter.geo_search(query: params[:search][:city_2], granularity: 'city').first
     city_2_geo = average_square get_coords(city_2)
 
     city_1_tweets = Twitter.search(params[:search][:query], geocode: city_1_geo.join(',') + ",25mi").statuses.collect(&:text)
     city_2_tweets = Twitter.search(params[:search][:query], geocode: city_2_geo.join(',') + ",25mi").statuses.collect(&:text)
 
-    @city_1_sentiments = city_1_tweets.collect do |tweet|
-      AlchemyAPI.search :sentiment_analysis, text: tweet
+    @cities = [city_1.name, city_2.name]
+
+    @sentiments = {}
+    @sentiments[city_1.name] = city_1_tweets.collect do |tweet|
+      AlchemyAPI.search(:sentiment_analysis, text: tweet).merge({'text' => tweet})
     end
 
-    @city_2_sentiments = city_2_tweets.collect do |tweet|
-      AlchemyAPI.search :sentiment_analysis, text: tweet
+    @sentiments[city_2.name] = city_2_tweets.collect do |tweet|
+      AlchemyAPI.search(:sentiment_analysis, text: tweet).merge({'text' => tweet})
     end
+
+    render action: :index
   end
 
   def average_square array
@@ -30,8 +35,8 @@ class SearchController < ApplicationController
     end
   end
 
-  def get_coords array
+  def get_coords obj
     # Why do these come in such tiny boxes and BACKWARDS?
-    array.first.bounding_box.coordinates.first.collect(&:reverse)
+    obj.bounding_box.coordinates.first.collect(&:reverse)
   end
 end
